3d8d78d3e26ee9a82161ab62f706e5fea949bb25
MiroslavGit <miroslav2022@gmail.com>
2026-04-27 17:16:30 +0200

    better table intities in shared components

--- packages/shared-types/src/index.ts
+++ packages/shared-types/src/index.ts
@@ -1,21 +1,14 @@
 export type { AuthMeResponse } from "./auth.js";
 
-export type { CreateRoomInviteResponse, CreateRoomResponse, GetRoomMembersResponse, GetRoomResponse, GetRoomsResponse, JoinRoomResponse, RoomMemberResponse, RoomMemberRole, RoomStatus, RoomSummaryResponse } from "./rooms.js";
-
 export type {
-  User as WatchPartyUser,
-  Room as WatchPartyRoom,
-  RoomStatus as WatchPartyRoomStatus,
-  RoomMember as WatchPartyRoomMember,
-  RoomMemberRole as WatchPartyRoomMemberRole,
-  Invite as WatchPartyInvite,
-  ChatMessage as WatchPartyChatMessage,
-  WebSocketConnection as WatchPartyWebSocketConnection,
-  PlaybackSnapshot as WatchPartyPlaybackSnapshot,
-  PlaybackState as WatchPartyPlaybackState,
-  ReactionEvent as WatchPartyReactionEvent,
-  ReactionTargetType as WatchPartyReactionTargetType,
-  ScheduledParty as WatchPartyScheduledParty,
-  ScheduledPartyStatus as WatchPartyScheduledPartyStatus,
-  IdempotencyEvent as WatchPartyIdempotencyEvent,
-} from "./watchparty/index.js";
+  CreateRoomInviteResponse,
+  CreateRoomResponse,
+  GetRoomMembersResponse,
+  GetRoomResponse,
+  GetRoomsResponse,
+  JoinRoomResponse,
+  RoomMemberResponse,
+  RoomMemberRole,
+  RoomStatus,
+  RoomSummaryResponse,
+} from "./rooms.js";
